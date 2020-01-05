e95754d528b1bc64170e81128f02388dd199a846
BorisMilenski <boris.milenski@abv.bg>
2020-01-05 17:21:28 +0200

    Used in client-server communication.

---
+++ src/Main/MessageIndex.java
@@ -0,0 +1,18 @@
+package Main;
+
+public enum MessageIndex {
+    LIST,
+    ACC,
+    CREATE,
+    PURCHASE,
+    INS,
+    EDIT,
+    DEL,
+    STARTBLACKFRIDAY,
+    ENDBLACKFRIDAY,
+    PRINT,
+    PRODUCTS,
+    ERRORPRIVILIGE,
+    ERRORQUANTITY,
+    ERRORSQL;
+}
